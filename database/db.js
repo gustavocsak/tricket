16d11bb6b1b7d91eab91c68ce130781c580b139d
gustavocsak <78715220+gustavocsak@users.noreply.github.com>
2022-04-07 22:05:03 -0800

    Added environment variables for database

--- database/db.js
+++ database/db.js
@@ -1,6 +1,8 @@
+let path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const mongoose = require('mongoose');
 
-let mongoDB = `mongodb+srv://gustavocs:admin@cluster0.5gryi.mongodb.net/Tricket?retryWrites=true&w=majority`;
+let mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.5gryi.mongodb.net/Tricket?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoDB);
 
